301570eb90f0761db20de5c0c09da57e34414294
Dr.V <lisa.vangsness@wichita.edu>
2020-06-09 13:26:54 -0500

    Base Code Push
    Pushed complete local files to GitHub project.

---
+++ Assets/_CompletedAssets/Scripts/Managers/EnemyManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace CompleteProject
+{
+    public class EnemyManager : MonoBehaviour
+    {
+        public PlayerHealth playerHealth;       // Reference to the player's heatlh.
+        //public GameObject enemy;              // The enemy prefab to be spawned.
+        //public float spawnTime = 3f;          // How long between each spawn.
+        public Transform[] spawnPoints;         // An array of spawn points the enemies can spawn from.
+        public GameObject[] enemy;              // An array of different enemy types.
+        public int enemyCount;
+
+        difficultyControl difficultyControl;
+
+        private void Awake()
+        {
+            difficultyControl = GameObject.Find("DifficultyController").GetComponent<difficultyControl>();
+        }
+
+        void Start ()
+        {
+            // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
+            InvokeRepeating ("Spawn", difficultyControl.spawnTime, difficultyControl.spawnTime);
+        }
+
+
+        void Spawn ()
+        {
+            // If the player has no health left and there are fewer than 30 enemies in gameplay...
+            if(playerHealth.currentHealth <= 0f || enemyCount >= 30)
+            {
+                // ... exit the function.
+                return;
+            }
+
+            // Find a random index between zero and one less than the number of spawn points.
+            int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+
+            // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+            Instantiate (enemy[Random.Range(0,enemy.Length)], spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+            // ... and increase the enemycount by 1.
+            enemyCount += 1;
+        }
+    }
+}
---
+++ Assets/_CompletedAssets/Scripts/Managers/difficultyControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class difficultyControl : MonoBehaviour
+{
+    //Difficulty parameters (default values)
+    public float spawnTime = 3f;            // The amount of time that ellapses between enemy spawns (3f).
+    public int enemyStartingHealth = 100;   // Enemy characters' starting health (100).
+    public int enemyAttackDamage = 10;      // The amount of health taken away per enemy attack (10).
+    public float enemySpeed = 2.5f;         // Enemy characters' speed (2.5f).
+    public int pointsEarned = 1;            // The number of points earned for eliminating enemies (1).
+    public float playerSpeed = 6f;          // The player's speed (6f).
+    public int lineOfSight = 30;            // The distance at which enemies can "see" the character (30).
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
