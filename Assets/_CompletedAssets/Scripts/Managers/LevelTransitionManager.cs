fadee67dd83fd46c12256bf1324ee20c710eb9d0
Dominic Canare <dom@greenlightgo.org>
2020-12-09 22:44:36 -0600

    Make level transitions less jarring

---
+++ Assets/_CompletedAssets/Scripts/Managers/LevelTransitionManager.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class LevelTransitionManager : MonoBehaviour
+{
+    public Canvas levelStartCanvas;
+    public Canvas levelFinishCanvas;
+    public Canvas screenFadeCanvas;
+
+    public PauseManager pauseManager;
+    public LoadScene loadSceneObject;
+
+    void Start()
+    {
+        if(levelStartCanvas)
+        {
+            Button readyButton = levelStartCanvas.GetComponentInChildren<Button>();
+            if(readyButton)
+            {
+                readyButton.onClick.AddListener(PlayerIsReadyToStart);
+                StartCoroutine(EnableButtonAfterDelay(3.0f, readyButton));
+            }
+        }
+
+        if(pauseManager)
+        {
+            pauseManager.enabled = false;
+        }
+
+        // start the game paused
+        Time.timeScale = 0;
+
+        if(loadSceneObject)
+        {
+            loadSceneObject.TimeOverEvent.AddListener(OnTimeOver);
+        }
+
+        if(levelFinishCanvas)
+        {
+            levelFinishCanvas.gameObject.SetActive(false);
+        }
+
+        FadeIn();
+    }
+
+    void PlayerIsReadyToStart()
+    {
+        StartCoroutine(FadeEffect.FadeCanvas(levelStartCanvas.gameObject, 1.0f, 0.0f, 0.125f, BeginPlay));
+    }
+
+    void BeginPlay(){
+        if(pauseManager)
+        {
+            Debug.Log("Pause manager re-enabled");
+            pauseManager.enabled = true;
+        }
+        levelStartCanvas.gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    void OnTimeOver()
+    {
+        if(pauseManager)
+        {
+            pauseManager.enabled = false;
+        }
+        Time.timeScale = 0;
+        StartCoroutine(FadeEffect.FadeCanvas(levelFinishCanvas.gameObject, 0.0f, 1.0f, 1.0f));
+
+        levelFinishCanvas.gameObject.SetActive(true);
+        Button readyButton = levelFinishCanvas.GetComponentInChildren<Button>();
+        if(readyButton)
+        {
+            readyButton.onClick.AddListener(PlayerIsReadyForNextLevel);
+            StartCoroutine(EnableButtonAfterDelay(2.0f, readyButton));
+        }
+    }
+
+    void PlayerIsReadyForNextLevel()
+    {
+        FadeOut(loadSceneObject.GotoNextLevel);
+    }
+
+    void RemoveScreenFadeCanvas()
+    {
+        if(screenFadeCanvas){
+            screenFadeCanvas.gameObject.SetActive(false);
+        }
+    }
+
+    void FadeIn()
+    {
+        if(screenFadeCanvas){
+            screenFadeCanvas.gameObject.SetActive(true);
+            StartCoroutine(FadeEffect.FadeCanvas(screenFadeCanvas.gameObject, 1.0f, 0.0f, 1.0f, RemoveScreenFadeCanvas));
+        }else{
+            RemoveScreenFadeCanvas();
+        }
+    }
+
+    void FadeOut(FadeEffect.FadeFinished Then = null)
+    {
+        if(screenFadeCanvas){
+            screenFadeCanvas.gameObject.SetActive(true);
+            StartCoroutine(FadeEffect.FadeCanvas(screenFadeCanvas.gameObject, 0.0f, 1.0f, 1.0f, Then));
+        }else{
+            if(Then != null){
+                Then();
+            }
+        }
+    }
+
+    IEnumerator EnableButtonAfterDelay(float timeout, Button button)
+    {
+        if(button)
+        {
+            button.GetComponent<CanvasGroup>().alpha = 0.0f;
+            button.enabled = false;
+
+            yield return new WaitForSecondsRealtime(timeout);
+
+            button.enabled = true;
+            EventSystem.current.SetSelectedGameObject(button.gameObject, null); // focus
+            StartCoroutine(FadeEffect.FadeCanvas(button.gameObject, 0.0f, 1.0f, 0.25f));
+        }
+    }
+}
--- Assets/_CompletedAssets/Scripts/Managers/LoadScene.cs
+++ Assets/_CompletedAssets/Scripts/Managers/LoadScene.cs
@@ -4,94 +4,112 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using CompleteProject;
 
+using UnityEngine.Events;
+
+
 public class LoadScene : MonoBehaviour
 {
-public static float TimeInLevel;
-public GameObject EnemySensor;
+  public float LevelDuration = 120;
+  public GameObject EnemySensor;
+
+  public UnityEvent TimeOverEvent = new UnityEvent();
+
+  protected float TimeInLevel;
 
 
-IEnumerator LoadScreen(){
-  yield return new WaitForSeconds(3);
-  if (SetConditions.playerLives == 0){
+
+  IEnumerator LoadScreen(){
+    yield return new WaitForSeconds(3);
+    if (SetConditions.playerLives == 0){
+      SetConditions.playerLives = 3;
+      SetConditions.level += 1;
+      SceneManager.LoadScene(1);
+    }
+    SceneManager.LoadScene(1);
+  }
+
+  IEnumerator LevelTimer(){
+    Debug.Log("StartTime: " + TimeInLevel);
+    while (TimeInLevel <= LevelDuration){
+        TimeInLevel += Time.deltaTime;
+        yield return null;
+    }
+
+    TimeOverEvent.Invoke();
+  }
+
+  public void GotoNextLevel(){
+    TimeInLevel = 0;
     SetConditions.playerLives = 3;
     SetConditions.level += 1;
     SceneManager.LoadScene(1);
   }
-  SceneManager.LoadScene(1);
-}
 
-IEnumerator LevelTimer(){
-  Debug.Log("StartTime: " + TimeInLevel);
-  while (TimeInLevel <= 120){
-      TimeInLevel += Time.deltaTime;
-      yield return null;
-  }
-  TimeInLevel = 0;
-  SetConditions.playerLives = 3;
-  SetConditions.level += 1;
-  SceneManager.LoadScene(1);
-}
-
-    // Start is called before the first frame update
-    void Start()
-    {
-      if (SetConditions.level % 5 == 0){
-        SetConditions.cueIndex += 1;
-      }
-
-      if (SetConditions.level == 21){
-        Application.Quit();
-      }
-      Debug.Log(SetConditions.cueCondition[SetConditions.cueIndex]);
-      Debug.Log(SetConditions.audioCondition);
-      SetCues(SetConditions.cueCondition[SetConditions.cueIndex], SetConditions.audioCondition);
-      switch (SceneManager.GetActiveScene().buildIndex){
-        case 2:
-        StartCoroutine("LoadScreen");
-        break;
-
-        case 1:
-        StartCoroutine("LevelTimer");
-        break;
-      }
+  // Start is called before the first frame update
+  void Start()
+  {
+    if (SetConditions.level % 5 == 0){
+      SetConditions.cueIndex += 1;
     }
 
-public void SetCues (string cueCondition, string audioCondition){
-  switch (cueCondition){
-    case "tactile":
-    EnemySensor.GetComponent<EnemySensor>().tactileCuesEnabled = true;
-    EnemySensor.GetComponent<EnemySensor>().visualCuesEnabled = false;
-    Debug.Log("Setting Cues");
-    break;
+    if (SetConditions.level == 21){
+      Application.Quit();
+    }
+    Debug.Log(SetConditions.cueCondition[SetConditions.cueIndex]);
+    Debug.Log(SetConditions.audioCondition);
+    SetCues(SetConditions.cueCondition[SetConditions.cueIndex], SetConditions.audioCondition);
+    switch (SceneManager.GetActiveScene().buildIndex){
+      case 2:
+      StartCoroutine("LoadScreen");
+      break;
 
-    case "visual":
-    EnemySensor.GetComponent<EnemySensor>().tactileCuesEnabled = false;
-    EnemySensor.GetComponent<EnemySensor>().visualCuesEnabled = true;
-    Debug.Log("Setting Cues");
-    break;
-
-    case "both":
-    EnemySensor.GetComponent<EnemySensor>().tactileCuesEnabled = true;
-    EnemySensor.GetComponent<EnemySensor>().visualCuesEnabled = true;
-    Debug.Log("Setting Cues");
-    break;
-
-    case "none":
-    EnemySensor.GetComponent<EnemySensor>().tactileCuesEnabled = false;
-    EnemySensor.GetComponent<EnemySensor>().visualCuesEnabled = false;
-    Debug.Log("Setting Cues");
-    break;
+      case 1:
+      StartCoroutine("LevelTimer");
+      break;
+    }
   }
 
-  switch(audioCondition){
-    case "Audio":
-    EnemySensor.GetComponent<EnemySensor>().auditoryCuesEnabled = true;
-    break;
+  public void SetCues (string cueCondition, string audioCondition){
+    EnemySensor enemySensorComponent = EnemySensor.GetComponent<EnemySensor>();
+    switch (cueCondition){
+      case "tactile":
+        enemySensorComponent.tactileCuesEnabled = true;
+        enemySensorComponent.visualCuesEnabled = false;
+        Debug.Log("Setting Cues");
+      break;
 
-    case "noAudio":
-    EnemySensor.GetComponent<EnemySensor>().auditoryCuesEnabled = false;
-    break;
+      case "visual":
+        enemySensorComponent.tactileCuesEnabled = false;
+        enemySensorComponent.visualCuesEnabled = true;
+        Debug.Log("Setting Cues");
+      break;
+
+      case "both":
+        enemySensorComponent.tactileCuesEnabled = true;
+        enemySensorComponent.visualCuesEnabled = true;
+        Debug.Log("Setting Cues");
+      break;
+
+      case "none":
+        enemySensorComponent.tactileCuesEnabled = false;
+        enemySensorComponent.visualCuesEnabled = false;
+        Debug.Log("Setting Cues");
+      break;
+    }
+
+    switch(audioCondition){
+      case "Audio":
+        enemySensorComponent.auditoryCuesEnabled = true;
+      break;
+
+      case "noAudio":
+        enemySensorComponent.auditoryCuesEnabled = false;
+      break;
+    }
   }
-}
 
+  public float GetTimeRemaining()
+  {
+    return LevelDuration - TimeInLevel;
+  }
 }
